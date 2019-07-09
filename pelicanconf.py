176135a41c1ebcfc7e273252b6f0f6c83fd60927
eijit <eiji765@gmail.com>
2019-07-10 00:08:08 +0900

    [add] Add articles.

--- pelicanconf.py
+++ pelicanconf.py
@@ -26,10 +26,23 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+    ('Twitter',   'https://twitter.com/eijit'),
+    ('GitHub',    'https://github.com/eijit'),
+    ('Facebook',  'https://www.facebook.com/eiji.taniguchi.56'),
+    ('Bookmeter', 'https://bookmeter.com/users/13752'),
+)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+PLUGIN_PATHS = ['../pelican-plugins']
+PLUGINS = [
+    'pelican-toc',
+    'render_math',
+    'series'
+]
+
+THEME = '../pelican-themes/elegant'
